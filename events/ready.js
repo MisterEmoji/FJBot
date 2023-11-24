4508f5d6e1026fdfad71d44c5d09184d7035ed6e
PomPon <filipakapompon@gmail.com>
2023-11-24 23:45:18 +0100

    transformed presence.json into profile.json

--- events/ready.js
+++ events/ready.js
@@ -4,12 +4,11 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.clear();
 		console.log("[INFO] Ready! logged in as " + client.user.tag);
 		client.user.setActivity("listening...", { type: "WATCHING" });
 
-		// set initial bot presence using presence.json
-		const presence = require("./../presence.json");
+		// set initial bot presence using profile.json
+		const presence = require("./../profile.json").presence;
 		client.user.setPresence(presence);
 	},
 };
