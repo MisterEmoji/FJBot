c80ed8fa6b79b42c00d25be7ec5d42f5533ef4cb
PomPon <filipakapompon@gmail.com>
2023-11-30 22:15:02 +0100

    small code improvements

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,11 @@
 const { Events } = require("discord.js");
+const profile = require("./../profile.json");
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.log("[INFO] Ready! logged in as " + client.user.tag);
-		client.user.setActivity("listening...", { type: "WATCHING" });
-
-		// set initial bot presence using profile.json
-		const presence = require("./../profile.json").presence;
-		client.user.setPresence(presence);
+		console.log(`[INFO] Ready! logged in as ${client.user.tag}`);
+		client.user.setPresence(profile.presence);
 	},
 };
