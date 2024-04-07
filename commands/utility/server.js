2e9d539e85ee5c364de590999edd762594c69537
Filip Gronkowski <filipakapompon@gmail.com>
2024-04-07 19:20:28 +0200

    update server cmd

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,25 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { EmbedBuilder } = require('discord.js');
-const { Client } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
-let serverInfoEmbed = new EmbedBuilder()
-serverInfoEmbed.setColor(0x00FFFF)
+let serverInfoEmbed = new EmbedBuilder();
+serverInfoEmbed.setColor(0x00ffff);
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Provides information about the server.'),
+		.setName("server")
+		.setDescription("Provides information about the server."),
 	async execute(interaction) {
-		let serverIcon = interaction.guild.iconURL();
-		console.log(serverIcon);
-		serverInfoEmbed.setAuthor(interaction.guild.name, serverIcon);
-		serverInfoEmbed.addFields(
-			{ name: 'Registered Users', value: `${interaction.guild.memberCount}`, inline: true },
-			{ name: 'Qualified as large?', value: `${interaction.guild.large}`,  inline: true }
-		)
-		serverInfoEmbed.setThumbnail(serverIcon);
-		
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply({embeds: [serverInfoEmbed]});
+		// construct server info embed
+		const serverInfo = new EmbedBuilder()
+			.setColor(0xffffff)
+			.setTitle(interaction.guild.name)
+			.setThumbnail(interaction.guild.iconURL())
+			.setDescription("description of the server.")
+			.addFields(
+				{ name: "Some title", value: "Some Value" },
+				{ name: "Inline title", value: "Inline value", inline: true }
+			);
+
+		await interaction.reply({ embeds: [serverInfo] });
 	},
-}; 
+};
